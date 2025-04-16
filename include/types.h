7d27ac3ad89c299e88e9230d9bd0f52452350e19
Noxmor <lord.noxmor@gmail.com>
2025-04-16 15:56:04 +0200

    Add types

---
+++ include/types.h
@@ -0,0 +1,26 @@
+#ifndef TQ_TYPES_H
+#define TQ_TYPES_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define TQ_FALSE 0
+#define TQ_TRUE 1
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef size_t usize;
+typedef u8 b8;
+typedef u16 b16;
+typedef u32 b32;
+typedef u64 b64;
+typedef float f32;
+typedef double f64;
+
+#endif
