5e8c2e78ebabbe45303dd310e06793fc26f23e57
Noxmor <lord.noxmor@gmail.com>
2025-04-16 23:16:59 +0200

    Add base mod

---
+++ base/data.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "interface",
+        name = "main-menu",
+        commands = {
+            { name = "CMD_OPTIONS" },
+            { name = "CMD_QUIT" },
+        }
+    }
+})
