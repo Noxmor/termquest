40e2f5bafa4b9a579b495663dc7a6fd50c179df0
Noxmor <lord.noxmor@gmail.com>
2025-04-17 14:09:28 +0200

    Implement prototype_registry_insert() and prototype_registry_find()

--- src/prototype_registry.c
+++ src/prototype_registry.c
@@ -1,10 +1,15 @@
 #include "prototype_registry.h"
+#include "hash.h"
+
+#include <stdlib.h>
+#include <string.h>
 
 #define TQ_PROTOTYPE_REGISTRY_CAPACITY 512
 
 typedef struct PrototypeRegistryEntry
 {
     void* proto;
+    const char* name;
     struct PrototypeRegistryEntry* next;
 } PrototypeRegistryEntry;
 
@@ -15,20 +20,47 @@ typedef struct PrototypeRegistry
 
 static PrototypeRegistry interface_registry;
 
-static void prototype_registry_insert(void* prototype, const char* name)
+static void* prototype_registry_find(PrototypeRegistry* registry,
+                                     const char* name)
 {
-    // TODO: Implement
+    u64 index = hash_djb2(name) % TQ_PROTOTYPE_REGISTRY_CAPACITY;
+    PrototypeRegistryEntry* entry = registry->entries[index];
+
+    while (entry != NULL)
+    {
+        if (strcmp(entry->name, name) == 0)
+        {
+            return entry->proto;
+        }
+    }
+
+    return NULL;
 }
 
-static void* prototype_registry_find(PrototypeRegistry* registry, const char* name)
+static void prototype_registry_insert(PrototypeRegistry* registry,
+                                      void* proto, const char* name)
 {
-    // TODO: Implement
-    return NULL;
+    void* found = prototype_registry_find(registry, name);
+    if (found != NULL)
+    {
+        // TODO: Overwrite existing prototype
+        return;
+    }
+
+    u64 index = hash_djb2(name) % TQ_PROTOTYPE_REGISTRY_CAPACITY;
+    PrototypeRegistryEntry* entry = registry->entries[index];
+
+    PrototypeRegistryEntry* new_entry = malloc(sizeof(PrototypeRegistryEntry));
+    new_entry->proto = proto;
+    new_entry->name = name;
+    new_entry->next = entry;
+
+    registry->entries[index] = new_entry;
 }
 
 void interface_registry_insert(Interface* inf)
 {
-    prototype_registry_insert(&interface_registry, inf->name);
+    prototype_registry_insert(&interface_registry, inf, inf->name);
 }
 
 Interface* interface_registry_find(const char* name)
