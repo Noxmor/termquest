cc26734fb9cd4d28bf5d7acf77304b232c1ab5b2
Noxmor <lord.noxmor@gmail.com>
2025-04-19 14:14:35 +0200

    Pop interface when CMD_BACK was executed

--- base/script.lua
+++ base/script.lua
@@ -2,6 +2,9 @@ local on_command_executed  = function(event)
     if event.name == "CMD_OPTIONS" then
         game.push_interface("options-menu")
     end
+    if event.name == "CMD_BACK" then
+        game.pop_interface()
+    end
     if event.name == "CMD_QUIT" then
         game.quit()
     end
